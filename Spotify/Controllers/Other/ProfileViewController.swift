bd1b1972dc4a0e6f4848f11cd9ae4ce75bd63d18
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2023-12-22 23:30:28 +0200

    update profile

--- Spotify/Controllers/Other/ProfileViewController.swift
+++ Spotify/Controllers/Other/ProfileViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import SDWebImage
 class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     private let tableView: UITableView = {
@@ -17,6 +17,7 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     }()
     
     private var models = [String]()
+    private let baseImageUser = "https://icon-library.com/images/no-user-image-icon/no-user-image-icon-0.jpg"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,8 +56,33 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         models.append("Email Address: \(model.email)")
         models.append("User ID: \(model.id)")
         models.append("Plan: \(model.product)")
+        if model.images.first?.url == nil{
+            createTableHeader(with: baseImageUser)
+        }
+        else{
+            createTableHeader(with: model.images.first?.url)
+        }
+        
         
         tableView.reloadData()
+    }
+    
+    private func createTableHeader(with string: String?){
+        guard let urlString = string, let url = URL(string: urlString) else {
+            return
+        }
+        let headerView = UIView(frame:  CGRect(x: 0, y: 0, width: view.width, height: view.width/1.5))
+        
+        let sizeImage: CGFloat = headerView.height/2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: sizeImage, height: sizeImage))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.contentMode = .scaleAspectFill
+        imageView.sd_setImage(with: url,completed: nil)
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = sizeImage/2
+        
+        tableView.tableHeaderView = headerView
     }
     
     private func failedToGetProfile(){
