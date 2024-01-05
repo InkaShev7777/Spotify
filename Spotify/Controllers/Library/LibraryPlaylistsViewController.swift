90dc90f6e14ab02b719fa665883884961c103a97
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-01-05 21:56:15 +0200

    add view for deleting tracks from playlist and bug fix

--- Spotify/Controllers/Library/LibraryPlaylistsViewController.swift
+++ Spotify/Controllers/Library/LibraryPlaylistsViewController.swift
@@ -157,6 +157,7 @@ extension LibraryPlaylistsViewController: UITableViewDelegate, UITableViewDataSo
         
         let vc = PlaylistViewController(playlist: playlist)
         vc.navigationItem.largeTitleDisplayMode = .never
+        vc.isOwner = true
         navigationController?.pushViewController(vc, animated: true)
         
     }
